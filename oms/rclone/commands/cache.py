34174d54ec096675b7b9fd8df861b25b16b279cc
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-02-28 23:11:53 +0530

    Code refactored

--- oms/rclone/commands/cache.py
+++ oms/rclone/commands/cache.py
@@ -1,45 +1,18 @@
-import logging
 from typing import Optional, Dict
 
-from .common import Command
-
-logger = logging.getLogger(__name__)
+from .abstracts import CommandAbstract
 
 
-class Cache:
+class Cache(CommandAbstract):
 	"""
 	Wrapper for `rclone cache`
 	"""
 
-	def __init__(self):
-		self.__cmd = Command()
-
-	@staticmethod
-	def __log(log_type: str, message: str):
-		"""
-		This method is to log all actions with a standard format
-
-		Args:
-			log_type (str): This should always be one of `info`, `warning`, `error`, `critical` or `debug`
-			message (str): Message that should be logged
-		"""
-		# Sanitize log_type
-		log_type = log_type.lower()
-
-		# Initialize logger string
-		logger_prefix = "[Config]"
-
-		if hasattr(logger, log_type):
-			getattr(logger, log_type)(f"{logger_prefix} {message}")
-		else:
-			# Invalid `log_type` passed
-			raise Exception(f"{logger_prefix} Invalid {log_type}!")
-
 	def expire(self, remote: str, with_data: Optional[bool] = False) -> Dict:
-		return self.__cmd.rc(command="cache/expire", parameters={"remote": remote, "withData": with_data})
+		return self.cmd.rc(command="cache/expire", parameters={"remote": remote, "withData": with_data})
 
 	def fetch(self) -> Dict:
-		return self.__cmd.rc(command="cache/fetch")
+		return self.cmd.rc(command="cache/fetch")
 
 	def stats(self) -> Dict:
-		return self.__cmd.rc(command="cache/stats")
+		return self.cmd.rc(command="cache/stats")
--- oms/rclone/commands/config.py
+++ oms/rclone/commands/config.py
@@ -1,12 +1,9 @@
-import logging
 from typing import Dict
 
-from .common import Command
-
-logger = logging.getLogger(__name__)
+from .abstracts import CommandAbstract
 
 
-class Config:
+class Config(CommandAbstract):
 	"""
 	Wrapper for `rclone config`
 	"""
@@ -15,32 +12,11 @@ class Config:
 		"""
 		:param obscure: Declare passwords are plain and need obscuring
 		"""
-		self.__cmd = Command()
+		super(Config, self).__init__()
 		self.__obscure = obscure
 
-	@staticmethod
-	def __log(log_type: str, message: str):
-		"""
-		This method is to log all actions with a standard format
-
-		Args:
-			log_type (str): This should always be one of `info`, `warning`, `error`, `critical` or `debug`
-			message (str): Message that should be logged
-		"""
-		# Sanitize log_type
-		log_type = log_type.lower()
-
-		# Initialize logger string
-		logger_prefix = "[Config]"
-
-		if hasattr(logger, log_type):
-			getattr(logger, log_type)(f"{logger_prefix} {message}")
-		else:
-			# Invalid `log_type` passed
-			raise Exception(f"{logger_prefix} Invalid {log_type}!")
-
 	def create(self, name: str, parameters: Dict, remote_type: str) -> Dict:
-		return self.__cmd.rc(command="config/create", parameters={
+		return self.cmd.rc(command="config/create", parameters={
 			"name": name,
 			"parameters": parameters,
 			"type": remote_type,
@@ -48,19 +24,19 @@ class Config:
 		})
 
 	def delete(self, name: str) -> Dict:
-		return self.__cmd.rc(command="config/delete", parameters={"name": name})
+		return self.cmd.rc(command="config/delete", parameters={"name": name})
 
 	def get(self, name: str) -> Dict:
-		return self.__cmd.rc(command="config/get", parameters={"name": name})
+		return self.cmd.rc(command="config/get", parameters={"name": name})
 
 	def list_remotes(self) -> Dict:
-		return self.__cmd.rc(command="config/listremotes")
+		return self.cmd.rc(command="config/listremotes")
 
 	def password(self, name: str, parameters: Dict) -> Dict:
-		return self.__cmd.rc(command="config/password", parameters={"name": name, "parameters": parameters})
+		return self.cmd.rc(command="config/password", parameters={"name": name, "parameters": parameters})
 
 	def providers(self) -> Dict:
-		return self.__cmd.rc(command="config/providers")
+		return self.cmd.rc(command="config/providers")
 
 	def update(self, name: str, parameters: Dict) -> Dict:
-		return self.__cmd.rc(command="config/update", parameters={"name": name, "parameters": parameters})
+		return self.cmd.rc(command="config/update", parameters={"name": name, "parameters": parameters})
